8ade41463f853fff17c8df6d176576300e2ba255
Berenice982708 <verdinesbere@gmail.com>
2020-05-19 22:49:58 -0500

    Favorite links

---
+++ keys.js
@@ -0,0 +1,11 @@
+module.exports = {
+
+    database: {
+        connectionLimit: 10,
+        host: 'sql9.freemysqlhosting.net',
+        user: 'sql9342003',
+        password: 'cgZBL1TEEE',
+        database: 'sql9342003'
+    }
+
+};
